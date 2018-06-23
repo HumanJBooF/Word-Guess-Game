5e1f64e6651e1270b3bd2da9d7eeaba35c782ae1
HumanJBooF <humanjboof@gmail.com>
2018-06-22 22:23:39 -0400

    basic html set up, some css!

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//Twin Peaks Theme peaksWords will hold all words have to do with Twin Peaks
+var peaksWords = 
+    [
+        'Woodsman',
+        'Agent Dale Cooper',
+        'Log Lady',
+        'Laura Palmer',
+        'Evil Coop',
+        'Dougie Jones',
+        'Albert',
+        'Gordon Cole',
+        'Major Briggs',
+        'Audrey Horne',
+        'James Hurley',
+        'One Armed Man',
+        'Diane',
+        'Norma Jennings',
+        'Big Ed',
+    ];
+    
