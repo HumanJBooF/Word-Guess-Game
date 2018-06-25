d76d371b68646ce532b84f2bf3742384254225af
HumanJBooF <humanjboof@gmail.com>
2018-06-25 15:55:55 -0400

    more comments and added a song for winning and losing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,22 +4,23 @@
     var peakWords = 
         [
             'woodsman',
-            'agent dale cooper',
-            'log lady',
-            'laura palmer',
-            'evil coop',
-            'dougie jones',
+            'agentdalecooper',
+            'loglady',
+            'laurapalmer',
+            'evilcoop',
+            'dougiejones',
             'albert',
-            'gordon cole',
-            'major briggs',
-            'audrey horne',
-            'james hurley',
-            'one armed man',
+            'gordoncole',
+            'majorbriggs',
+            'audreyhorne',
+            'jameshurley',
+            'onearmedman',
             'diane',
-            'norma jennings',
-            'big ed',
+            'normajennings',
+            'biged',
         ];
-        //Empty to store the random gen word
+
+        //Empty to store the random  peaksword
         var currentWord = "";
         //holds letters of currentWord
         var currentLetters = [];
@@ -29,24 +30,25 @@
         var answer = [];    
         //empty to hold all wrong guesses, and show user guess
         var wrongLtrs = [];    
+        
 
         //Stats
         var wins = 0;
         var losses = 0;
-        var guessesRemaining = 10;
+        var guessesRemaining = 12;
         
-
+//load with webpage
  window.onload = function() {
     document.getElementById("underscore").innerHTML = answer.join(" ");
-    document.getElementById("remaining").innerHTML = guessesRemaining;
+    document.getElementById("remaining").innerHTML ="guesses remaing " + guessesRemaining;
     document.getElementById("wins").innerHTML = "Wins " + " " + wins;
     document.getElementById("losses").innerHTML = "Losses " + " " + losses;
+    document.getElementById("press").innerHTML = "Press any key to get started!";
+    
+}
 
 
- }
-
-
-        
+    //function for game start and reset
      function startGame() {
 		//Computer selects a word from the array
 		currentWord = peakWords[Math.floor(Math.random() * peakWords.length)];
@@ -60,36 +62,24 @@
 		numBlanks = currentLetters.length;
             console.log(numBlanks);
             
-            guessesRemaining = 10;
-            wrongLtrs = [];
-            answer = [];
+            guessesRemaining = 12; //sets the number to 12
+            wrongLtrs = [];        //empty array for the wrong guesse
+            answer = [];           //empty array for the underscores
 
             for(i = 0; i < numBlanks; i++) {
                 answer.push("_");
-                
-            }
-    
+                }
 
-    
+           }
 
-        }
-
-        
-
-
-        
-        
-        
-        
-        
-        
-        
-        function compare(letter) {
+           
+            //function to compare user guess to word
+            function compare(letter) {
              //check it pressed key is a letter
              if(event.keyCode >= 65 && event.keyCode <= 90) { //if its part of the alphabet 
-
+            
                 //check if the letter is in the word
-                    var correctLetter = false; 
+                var correctLetter = false;
     
                     for(var i = 0; i < numBlanks; i++) { 
                         if(currentWord[i] == letter) {
@@ -104,7 +94,8 @@
                             }
                         }
                     }
-                    //if it is not in the word
+                   
+                    //if it is not in the word push to wrongLtrs
                     else {
                         wrongLtrs.push(letter);
                         guessesRemaining--
@@ -114,7 +105,7 @@
             
                }
       
-
+               //function to update wins and losses
                function rounds() {
                    
                 
@@ -122,8 +113,10 @@
                 document.getElementById("underscore").innerHTML = answer.join(" ");
                 document.getElementById("guessed").innerHTML = "Already used: " + " " + wrongLtrs.join(" ");
     
+                
                 if(currentLetters.toString() == answer.toString()) {
                     wins++;
+                    document.getElementById("coffee").play();
                     alert("Have a nice warm slice of cherry pie and hot cup of black coffee!");
                     
                     //update wins
@@ -134,31 +127,33 @@
                 }
                 else if (guessesRemaining == 0) {
                     losses++;
-    
+                    document.getElementById("amateur").play();
+                    alert("The evil spirit Bob took over your soul... you lose!");
                     
-                // Update the wins in the HTML doc
+                    startGame();
+
+
+                // Update the losses in the HTML doc
                 document.getElementById("losses").innerHTML = "Losses: " + " " + losses;
-    
+                    
                 
-                startGame();
+                
                 document.getElementById("guessed").innerHTML = "Letters Already Guessed:" + " " + " ";
-    
+                
                 }
            } 
         
-            
-
 
 
     startGame(); 
-            
+    
        //get input on what keys are being pressed
        document.onkeyup = function(event) {
-   
+        //put the key pressed into ltrsGuess
         var ltrsGuess = String.fromCharCode(event.keyCode).toLowerCase();
-            
-            compare(ltrsGuess);
-            rounds();
+        //send the letter to the compare loop
+        compare(ltrsGuess);
+        rounds(); 
             
        }
     
